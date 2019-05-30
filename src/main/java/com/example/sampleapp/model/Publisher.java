6b33b05feced173b2fd1d6a09d2f4faa7f93e4b6
Himal Shakya <shakyahimal1@gmail.com>
2019-05-29 20:52:28 -0400

    First Commit

---
+++ src/main/java/com/example/sampleapp/model/Publisher.java
@@ -0,0 +1,9 @@
+package com.example.sampleapp.model;
+
+public class Publisher {
+    private String name;
+    private String streetName;
+    private String city;
+    private String state;
+    private String zip;
+}
